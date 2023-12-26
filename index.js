6bbe19d785b0f68ce5d65061acb0d33e130f13e1
Mwaqas786 <onidea789@gmail.com>
2023-12-26 17:30:59 +0500

    jawascript lurning

---
+++ index.js
@@ -0,0 +1,8 @@
+let result="124"
+console.log(typeof result)
+let convetNumber=Number(result)
+console.log(typeof convetNumber);
+
+//"34"conver in 33
+//"34avb" not a number
+//true convert is 1 false is convert 0
